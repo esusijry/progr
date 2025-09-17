0012cbfc63ee114f945d0f375fa30c0c569baf54
Софрыгина Олеся <ls.fdrva@gmail.com>
2025-09-17 19:48:28 +0500

    Код с лекции 1

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Привет, мир!");
+            Console.ResetColor();
+
+            Console.ReadKey();
+        }
+    }
+}
