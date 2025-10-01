0492ff63aad8a0fd62d9c378dce43ceeda5293a7
Софрыгина Олеся <ls.fdrva@gmail.com>
2025-10-01 19:58:31 +0500

    task03

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine ("Введите трехзначное число");
+
+            var n = int.Parse(Console.ReadLine());
+
+            var hundreds = n / 100;
+            var tents = (n / 10) % 10;
+            var units = n % 10;
+
+            var result = hundreds + tents * 10 + units * 100;
+            Console.WriteLine("Ответ: " + result);
+
+        }
+
+        static int InputInteger(string message)
+        { 
+            
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
