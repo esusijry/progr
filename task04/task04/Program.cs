6b24c56d9c5d577d089d9f74d28b004a11aade1b
Софрыгина Олеся <ls.fdrva@gmail.com>
2025-10-07 15:23:42 +0500

    task04

---
+++ task04/task04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число x");
+
+            var x = double.Parse(Console.ReadLine());
+
+            var y = F(x);
+
+            Console.WriteLine("y = " + y);
+
+        }
+
+        static double F(double x) => 2 * Math.Sin(Math.Sqrt(Math.Pow(x, 2) + 4) / 2) * Math.Cos(Math.Sqrt(Math.Pow(x, 2) + 1) / 2);
+
+    }
+}
