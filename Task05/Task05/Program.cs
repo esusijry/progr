15133679e5acc6e879929f8a11e62f3c1c0190ed
Софрыгина Олеся <ls.fdrva@gmail.com>
2025-10-08 20:24:12 +0500

    task05

---
+++ Task05/Task05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Calculate(2, 3) + 1/Calculate(5, 7); 
+            Console.WriteLine(a);
+        }
+        static double Calculate(double x, double y) => 
+            (Math.Sin(x) + Math.Sin(y)) / (Math.Cos(x) + Math.Cos(y));
+    }
+}
